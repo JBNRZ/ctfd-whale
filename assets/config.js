af645049baf2660e79f597749e39b23555121887
JBNRZ <3537659915@qq.com>
2024-03-11 22:54:36 +0800

    Fix bug
    适配 CTFd 3.7.0

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,25 @@
 const $ = CTFd.lib.$;
 
+
+function getFormJson(form) {
+    var o = {};
+    var a = $(form).serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
 $(".config-section > form:not(.form-upload)").submit(async function (event) {
     event.preventDefault();
-    const obj = $(this).serializeJSON();
+    const obj = getFormJson($(this))
     const params = {};
     for (let x in obj) {
         if (obj[x] === "true") {
